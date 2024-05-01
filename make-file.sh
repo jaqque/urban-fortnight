ece4151d298394adafb444dad5a79f3bd7046947
jaqque <jaqque@gmail.com>
2024-05-01 13:31:43 -0700

    Support both platforms!
    macOS and GNU/Linux!

--- make-file.sh
+++ make-file.sh
@@ -7,12 +7,20 @@ temp=.file.tmp
 target_size=2147483648 # 2 * 2 ** ( 3 * 10 ) # eg: 2GiB
 lines_in_small=10
 
+# select proper stat(1) arguments
+case "$(uname -s)" in
+  Darwin) stat_arg='-f%z' ;;
+  Linux) stat_arg='-c%s' ;;
+  *) printf 'Here%ss a nickel kid. Go buy yourself a real computer.\n' "'"; 
+     exit 5 ;;
+esac
+
 if ! [ -f "$output" ]; then
   # setup
   cp "$input" "$output"
 
   # duplicate until 2+G
-  while [ "$(stat -f%z "$output")" -lt "$target_size" ]; do
+  while [ "$(stat "$stat_arg" "$output")" -lt "$target_size" ]; do
     cat "$output" "$output" > "$temp"
     mv "$temp" "$output"
     printf .
